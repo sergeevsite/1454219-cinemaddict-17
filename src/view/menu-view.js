7ce4cf7d12717360b75cf53a7b9f4957a7ed9db2
sergeev_a <sergeev_a@joy.money>
2022-05-10 18:33:32 +0700

    Поправил стайл-код шаблонных строк

--- src/view/menu-view.js
+++ src/view/menu-view.js
@@ -1,6 +1,7 @@
 import {createElement} from '../render';
 
-const createMenuTemplate = () => `<nav class="main-navigation">
+const createMenuTemplate = () =>
+  `<nav class="main-navigation">
     <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
     <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
     <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
